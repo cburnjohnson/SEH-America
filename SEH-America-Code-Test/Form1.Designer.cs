36c767e0d8a1073618c007fb7874fb906b90e0e5
Cole Johnson <cburnjohnson@gmail.com>
2020-04-14 12:38:37 -0700

    initial commit

---
+++ SEH-America-Code-Test/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace SEH_America_Code_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.titleInput = new System.Windows.Forms.TextBox();
+            this.bodyText = new System.Windows.Forms.TextBox();
+            this.suggestImagesBtn = new System.Windows.Forms.Button();
+            this.generateSlide = new System.Windows.Forms.Button();
+            this.imageOne = new System.Windows.Forms.PictureBox();
+            this.imageTwo = new System.Windows.Forms.PictureBox();
+            this.imageThree = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageThree)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(632, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Power Point Slide Generator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(111, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Title:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(111, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Text:";
+            // 
+            // titleInput
+            // 
+            this.titleInput.Location = new System.Drawing.Point(192, 125);
+            this.titleInput.Name = "titleInput";
+            this.titleInput.Size = new System.Drawing.Size(562, 20);
+            this.titleInput.TabIndex = 3;
+            // 
+            // bodyText
+            // 
+            this.bodyText.Location = new System.Drawing.Point(192, 186);
+            this.bodyText.Multiline = true;
+            this.bodyText.Name = "bodyText";
+            this.bodyText.Size = new System.Drawing.Size(562, 82);
+            this.bodyText.TabIndex = 4;
+            this.bodyText.TextChanged += new System.EventHandler(this.bodyText_TextChanged);
+            // 
+            // suggestImagesBtn
+            // 
+            this.suggestImagesBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.suggestImagesBtn.Location = new System.Drawing.Point(85, 287);
+            this.suggestImagesBtn.Name = "suggestImagesBtn";
+            this.suggestImagesBtn.Size = new System.Drawing.Size(100, 43);
+            this.suggestImagesBtn.TabIndex = 5;
+            this.suggestImagesBtn.Text = "Suggest Images";
+            this.suggestImagesBtn.UseVisualStyleBackColor = true;
+            this.suggestImagesBtn.Click += new System.EventHandler(this.suggestImagesBtn_Click);
+            // 
+            // generateSlide
+            // 
+            this.generateSlide.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.generateSlide.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateSlide.Location = new System.Drawing.Point(191, 489);
+            this.generateSlide.Name = "generateSlide";
+            this.generateSlide.Size = new System.Drawing.Size(562, 43);
+            this.generateSlide.TabIndex = 7;
+            this.generateSlide.Text = "GENERATE SLIDE";
+            this.generateSlide.UseVisualStyleBackColor = true;
+            this.generateSlide.Click += new System.EventHandler(this.generateSlide_Click);
+            // 
+            // imageOne
+            // 
+            this.imageOne.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.imageOne.Location = new System.Drawing.Point(191, 287);
+            this.imageOne.Name = "imageOne";
+            this.imageOne.Size = new System.Drawing.Size(192, 173);
+            this.imageOne.TabIndex = 0;
+            this.imageOne.TabStop = false;
+            this.imageOne.Click += new System.EventHandler(this.imageOne_Click);
+            // 
+            // imageTwo
+            // 
+            this.imageTwo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.imageTwo.Location = new System.Drawing.Point(390, 287);
+            this.imageTwo.Name = "imageTwo";
+            this.imageTwo.Size = new System.Drawing.Size(204, 173);
+            this.imageTwo.TabIndex = 8;
+            this.imageTwo.TabStop = false;
+            this.imageTwo.Click += new System.EventHandler(this.imageTwo_Click);
+            // 
+            // imageThree
+            // 
+            this.imageThree.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.imageThree.Location = new System.Drawing.Point(600, 287);
+            this.imageThree.Name = "imageThree";
+            this.imageThree.Size = new System.Drawing.Size(211, 173);
+            this.imageThree.TabIndex = 9;
+            this.imageThree.TabStop = false;
+            this.imageThree.Click += new System.EventHandler(this.imageThree_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 572);
+            this.Controls.Add(this.imageThree);
+            this.Controls.Add(this.imageTwo);
+            this.Controls.Add(this.imageOne);
+            this.Controls.Add(this.generateSlide);
+            this.Controls.Add(this.suggestImagesBtn);
+            this.Controls.Add(this.bodyText);
+            this.Controls.Add(this.titleInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageThree)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox titleInput;
+        private System.Windows.Forms.TextBox bodyText;
+        private System.Windows.Forms.Button suggestImagesBtn;
+        private System.Windows.Forms.Button generateSlide;
+        private System.Windows.Forms.PictureBox imageOne;
+        private System.Windows.Forms.PictureBox imageTwo;
+        private System.Windows.Forms.PictureBox imageThree;
+    }
+}
+
---
+++ SEH-America-Code-Test/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Syncfusion.Presentation;
+using System.Net.Http;
+
+namespace SEH_America_Code_Test
+{
+    public partial class Form1 : Form
+    {
+        string imageUrlOne = "";
+        string imageUrlTwo = "";
+        string imageUrlThree = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void suggestImagesBtn_Click(object sender, EventArgs e)
+        {
+            string titleText = titleInput.Text;
+            string[] titleTextArr = titleText.Split(null);
+
+            string queryString = string.Join("+", titleTextArr);
+
+
+
+            string url = "https://pixabay.com/api/?key=16044426-244c292e5e138059720ae50ee&q=" + queryString;
+
+
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string content = new StreamReader(response.GetResponseStream()).ReadToEnd();
+         
+
+            var jo = JObject.Parse(content);
+
+            
+            if (Convert.ToInt32(jo["totalHits"]) == 0)
+            {
+                MessageBox.Show("No available images");
+            } else
+            {
+                var imageOneUrl = jo["hits"][0]["webformatURL"].ToString();
+                var imageTwoUrl = jo["hits"][1]["webformatURL"].ToString();
+                var imageThreeUrl = jo["hits"][2]["webformatURL"].ToString();
+
+
+
+
+
+                imageOne.Load(imageOneUrl);
+                imageTwo.Load(imageTwoUrl);
+                imageThree.Load(imageThreeUrl);
+
+                imageOne.SizeMode = PictureBoxSizeMode.StretchImage;
+                imageTwo.SizeMode = PictureBoxSizeMode.StretchImage;
+                imageThree.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            
+
+        }
+
+        private void generateSlide_Click(object sender, EventArgs e)
+        {
+            //Creates PowerPoint Presentation
+            IPresentation pptxDoc = Presentation.Create();
+            //Adds slide to the PowerPoint
+            ISlide slide = pptxDoc.Slides.Add(SlideLayoutType.Blank);
+            //Adds textbox to the slide
+            IShape textboxShape = slide.AddTextBox(0, 0, 1024, 100);
+            IShape textboxShape2 = slide.AddTextBox(0, 101, 1024, 200);
+            
+
+            if (imageUrlOne != "") 
+            {
+                WebClient myWebClient = new WebClient();
+                byte[] bytes = myWebClient.DownloadData(imageUrlOne);
+
+                Stream pictureStream = new MemoryStream(bytes);
+
+                IPicture picture = slide.Pictures.AddPicture(pictureStream, 50, 301, 150, 150);
+
+                pptxDoc.Save(titleInput.Text + ".pptx");
+
+                pictureStream.Dispose();
+            }
+
+            if (imageUrlTwo != "")
+            {
+                WebClient myWebClient = new WebClient();
+                byte[] bytes = myWebClient.DownloadData(imageUrlTwo);
+
+                Stream pictureStream = new MemoryStream(bytes);
+
+                IPicture picture = slide.Pictures.AddPicture(pictureStream, 250, 301, 150, 150);
+
+                pptxDoc.Save(titleInput.Text + ".pptx");
+
+                pictureStream.Dispose();
+            }
+
+            if (imageUrlThree != "")
+            {
+                WebClient myWebClient = new WebClient();
+                byte[] bytes = myWebClient.DownloadData(imageUrlThree);
+
+                Stream pictureStream = new MemoryStream(bytes);
+
+                IPicture picture = slide.Pictures.AddPicture(pictureStream, 450, 301, 150, 150);
+
+                pptxDoc.Save(titleInput.Text + ".pptx");
+
+                pictureStream.Dispose();
+            }
+
+            //Adds paragraph to the textbody of text box
+            IParagraph title = textboxShape.TextBody.AddParagraph();
+            IParagraph body = textboxShape2.TextBody.AddParagraph();
+            //Adds a TextPart to the paragraph
+            ITextPart textPart = title.AddTextPart();
+            ITextPart textPart2 = body.AddTextPart();
+            //Adds text to the TextPart
+            textPart.Text = titleInput.Text;
+            textPart2.Text = bodyText.Text;
+            // Formatting
+            title.HorizontalAlignment = HorizontalAlignmentType.Center;
+            body.HorizontalAlignment = HorizontalAlignmentType.Center;
+            IFont titleFont = title.Font;
+            titleFont.Bold = true;
+            titleFont.FontSize = 40;
+
+
+            //Saves the Presentation
+            pptxDoc.Save(titleInput.Text + ".pptx");
+            //Closes the Presentation
+            pptxDoc.Close();
+
+
+
+
+        }
+
+        private void bodyText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageOne_Click(object sender, EventArgs e)
+        {
+            if (imageOne.BorderStyle == BorderStyle.Fixed3D)
+            {
+                imageUrlOne = "";
+                imageOne.BorderStyle = BorderStyle.None;
+            } else
+            {
+                imageUrlOne = Convert.ToString(imageOne.ImageLocation);
+                imageOne.BorderStyle = BorderStyle.Fixed3D;
+            }
+            
+            
+
+        }
+
+       
+
+
+        private void imageTwo_Click(object sender, EventArgs e)
+        {
+            
+
+            if (imageTwo.BorderStyle == BorderStyle.Fixed3D)
+            {
+                imageUrlTwo = "";
+                imageTwo.BorderStyle = BorderStyle.None;
+            }
+            else
+            {
+                imageUrlTwo = Convert.ToString(imageTwo.ImageLocation);
+                imageTwo.BorderStyle = BorderStyle.Fixed3D;
+            }
+        }
+
+        private void imageThree_Click(object sender, EventArgs e)
+        {
+            if (imageThree.BorderStyle == BorderStyle.Fixed3D)
+            {
+                imageUrlThree = "";
+                imageThree.BorderStyle = BorderStyle.None;
+            }
+            else
+            {
+                imageUrlThree = Convert.ToString(imageThree.ImageLocation);
+                imageThree.BorderStyle = BorderStyle.Fixed3D;
+            }
+        }
+    }
+}
